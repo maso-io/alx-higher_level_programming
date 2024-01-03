a2c97768c4327e0f97ebab05ecacfe8acca36c8e
Maso <nkombisa.masonwabe@gmail.com>
2024-01-04 00:49:14 +0200

    fizzbuzz

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+def fizzbuzz():
+	for i in range(1, 101):
+		if i % (3 * 5) == 0:
+			print("FizzBuzz")
+		elif (i % 5) == 0:
+			print("Buzz")
+		elif (i % 3) == 0:
+			print("Fizz")
+		else:
+			print("{}".format(i))
