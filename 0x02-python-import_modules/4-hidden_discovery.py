29a66db4abce7219d40dcfdddfc02aafd7ff0dc7
Maso <nkombisa.masonwabe@gmail.com>
2024-01-05 01:21:39 +0200

    add_script 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    import hidden_4
+
+    # get hidden_4 namespace
+    names = dir(hidden_4)
+
+    # filter out names starting with `__`
+    filtered_names = [name for name in names if not name.startswith("__")]
+    sorted_names = sorted(filtered_names)
+
+    for name in names:
+        print(name)
