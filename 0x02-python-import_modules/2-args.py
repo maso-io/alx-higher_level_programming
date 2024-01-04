1fdc40f398dc5d77158b55424e8b89340e5c1a94
Maso <nkombisa.masonwabe@gmail.com>
2024-01-05 01:06:27 +0200

    mod-2-print_format

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,12 +5,12 @@ if __name__ == "__main__":
 
    i = len(sys.argv)
    if i == 1:
-       print(f'0 arguments.')
+       print('0 arguments.')
    else:
        if i == 2:
-           print(f'1 argument:')
-           print(f'1: {sys.argv[1]}')
+           print('1 argument:')
+           print('1: {}'.format(sys.argv[1]))
        else:
-           print(f'{i - 1:d} arguments:')
+           print('{:d} arguments:'.format(i - 1))
            for x in range(1, i):
-               print(f'{x:d}: {sys.argv[x]}')
+               print('{:d}: {}'.format(x, sys.argv[x]))
