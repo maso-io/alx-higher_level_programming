a992ecbcf374b3ba08bfb8b013e2670d158dda4b
Maso <nkombisa.masonwabe@gmail.com>
2024-01-05 01:08:30 +0200

    pycodestyle mod

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-   import sys
+    import sys
 
-   i = len(sys.argv)
-   if i == 1:
-       print('0 arguments.')
-   else:
-       if i == 2:
-           print('1 argument:')
-           print('1: {}'.format(sys.argv[1]))
-       else:
-           print('{:d} arguments:'.format(i - 1))
-           for x in range(1, i):
-               print('{:d}: {}'.format(x, sys.argv[x]))
+    i = len(sys.argv)
+    if i == 1:
+        print('0 arguments.')
+    else:
+        if i == 2:
+            print('1 argument:')
+            print('1: {}'.format(sys.argv[1]))
+        else:
+            print('{:d} arguments:'.format(i - 1))
+            for x in range(1, i):
+                print('{:d}: {}'.format(x, sys.argv[x]))
