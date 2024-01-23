038722210ae81643e8c296656dacf93939144c83
Maso <nkombisa.masonwabe@gmail.com>
2024-01-23 03:53:50 +0200

    add IE

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -5,5 +5,8 @@ def safe_print_list_integers(my_list=[], x=0):
             print("{:d}".format(my_list[i]), end="")
     except (ValueError, TypeError):
         pass
+    except IndexError:
+        print()
+        return i
     print()
     return i + 1
