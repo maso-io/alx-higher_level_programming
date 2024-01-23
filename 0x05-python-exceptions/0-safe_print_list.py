0c0d67485c9916128d229a1bf730b1f12fdbd5d8
Maso <nkombisa.masonwabe@gmail.com>
2024-01-23 03:00:36 +0200

    mod perm

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,10 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     for i in range(x):
         try:
             print("{}".format(my_list[i]), end="")
         except IndexError:
-            print()
+            print("".format())
             return i
-    print()
+    print("".format())
     return i + 1
