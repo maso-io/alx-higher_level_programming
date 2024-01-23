806e4303c14c1c76a4cd316a15702d7924494ed9
Maso <nkombisa.masonwabe@gmail.com>
2024-01-23 03:31:43 +0200

    add file 2 mod

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    try:
+        for i in range(x):
+            print("{:d}".format(my_list[i]))
+    except (ValueError):
+        return i
+    return i + 1
