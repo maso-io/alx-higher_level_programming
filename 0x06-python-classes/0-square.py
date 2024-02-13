0c68de266c296a1b35fc173e9825273096f39e91
Maso <nkombisa.masonwabe@gmail.com>
2024-02-13 10:42:54 +0200

    module documentation 1.1

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 class Square():
-    """
+    '''
         Class that represents an empty square
-    """
+    '''
     pass
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 class Square:
-    """
+    '''
         Class that represents a square of a defined `size`
-    """
+    '''
     def __init__(self, size):
-        """
-            Initializes an instance of the class.
+        '''Initializes an instance of the class.
 
             Args:
             size (float): The size of the square
-        """
+        '''
         self._size = size
